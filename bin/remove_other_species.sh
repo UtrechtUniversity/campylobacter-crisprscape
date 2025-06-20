c5af7371140237bdc05d21daafb5855e2a7ef1fa
Sam Nooij <s.nooij@uu.nl>
2025-06-20 15:04:31 +0200

    Use variables for output file names

--- bin/remove_other_species.sh
+++ bin/remove_other_species.sh
@@ -2,22 +2,25 @@
 set -euo pipefail
 IFS=$'\n'
 
+downloaded_samples="data/tmp/all_downloaded_samples.txt"
+other_species="data/tmp/other_species.txt"
+
 # Get all accession IDs from all batches
 for batch in data/tmp/ATB/batch_*
 do
     ls ${batch} | sed 's/.fa//g'
-done > data/tmp/ATB/all_samples.txt
+done > ${downloaded_samples}
 
 # Compare the accession IDs with those from your species of interest -
 #  saving only the other ones (not of interest)
-comm -23 <(sort data/tmp/ATB/all_samples.txt) <(sort data/ATB/all_samples_of_interest.txt) > data/tmp/other_species.txt
+comm -23 <(sort ${downloaded_samples}) <(sort data/ATB/all_samples_of_interest.txt) > ${other_species}
 
-echo -e "Removing $(wc -l data/tmp/other_species.txt) fasta files..."
+echo -e "Removing $(wc -l ${other_species}) fasta files..."
 
 # Use the list of non-Campylobacters to remove unnecessary fasta files
 while read sample;
 do
     rm data/tmp/ATB/batch_*/${sample}.fa
-done < data/tmp/other_species.txt
+done < ${other_species}
 
 echo "Done!"
