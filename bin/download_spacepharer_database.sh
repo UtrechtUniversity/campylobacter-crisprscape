4e94775186cf79c574359cfc83e18f51ad4c3c39
Necopy-byte <g.s.a.vanwaltmeijer@students.uu.nl>
2025-07-11 16:10:16 +0200

    Rework spacepharer database download

--- bin/download_spacepharer_database.sh
+++ bin/download_spacepharer_database.sh
@@ -4,12 +4,12 @@
 ## They are then also extracted and specifically for Phagescope merged into one database for use in Spacepharer. 
 ##
 ## Note: RUN FROM THE BASE FOLDER AS IT WILL NOT WORK OTHERWISE
-## Usage: download_spacepharer_database.sh
-## TODO: only uses one thread and can take very long for phagescope (unzipping the very large tar files), need to parallelise.
-
+## Usage: download_spacepharer_database.sh [threads]
+## [threads]: the amount of threads used for extracting and merging phagescope data. can at maximum use 14 threads, default is 1. 
+threads="${1:-"1"}"
 #PLSDB download
 mkdir -p data/raw/PLSDB
-if test -f data/raw/PLSDB/download_meta.tar.gz; then
+if  [ -f data/raw/PLSDB/download_meta.tar.gz ]; then
     echo "Already downloaded PLSDB, skipping..."
 else
     echo "Downloading PLSDB"
@@ -17,8 +17,11 @@ else
 fi
 
 echo "Extracting download"
-tar -xzf data/raw/PLSDB/download_meta.tar.gz -C data/raw/PLSDB/
-
+if [ -f data/raw/PLSDB/sequences.fasta.bz2 ]; then
+    echo "already extracted, skipping..."
+else
+    tar -xzf data/raw/PLSDB/download_meta.tar.gz -C data/raw/PLSDB/
+fi
 echo "Unzipping sequences"
 bzip2 -d data/raw/PLSDB/sequences.fasta.bz2
 
@@ -38,19 +41,22 @@ for DB in "Genbank" "RefSeq" "DDBJ" "EMBL" "PhagesDB" "GPD" "GVD" "MGV" "TemPhD"
 done
 
 echo "Extracting databases"
+parallel --jobs "$threads" 'DB={}; path=data/raw/phagescope; [ -f "$path/$DB.fasta" ] || [ -d "$path/$DB" ] && \
+echo "$DB already extracted, skipping..." || \
+( echo "extracting $DB"; tar -xzf "$path/$DB.tar.gz" -C "$path/" )' ::: Genbank RefSeq DDBJ EMBL PhagesDB GPD GVD MGV TemPhD CHVD IGVD IMG_VR GOV2 STV
 
-for DB in "Genbank" "RefSeq" "DDBJ" "EMBL" "PhagesDB" "GPD" "GVD" "MGV" "TemPhD" "CHVD" "IGVD" "IMG_VR" "GOV2" "STV"
-    do
-    echo "extracting $DB"
-    tar -xzf "data/raw/phagescope/$DB.tar.gz" -C data/raw/phagescope/
+echo "merging phagescope sequences"
+parallel --jobs "$threads" 'DB={}; path=data/raw/phagescope; [ -f "${path}/${DB}.fasta" ] && \
+echo "$DB already merged, skipping..." || \
+( echo "merging $DB"; genomes=$(find "$path/$DB" -type f -name "*.fasta"); > $path/$DB.fasta ; for files in $genomes; do cat $files >> $path/$DB.fasta; done)' \
+::: Genbank RefSeq DDBJ EMBL PhagesDB GPD GVD MGV TemPhD
 
-done
 
 echo "Downloading Phagescope metadata"
 
 for DB in "genbank" "refseq" "ddbj" "embl" "phagesdb" "gpd" "gvd" "mgv" "temphd" "chvd" "igvd" "img_vr" "gov2" "stv"
     do
-    if test -f "data/raw/phagescope/$DB_phage_meta_data.tsv"; then
+    if [ -f "data/raw/phagescope/${DB}_phage_meta_data.tsv" ]; then
         echo "Already downloaded $DB, skipping..."
     else
         echo "Downloading $DB"
@@ -60,17 +66,18 @@ done
 
 echo "Merging phagescope metadata"
 first=true
-for DB in "genbank" "refseq" "ddbj" "embl" "phagesdb" "gpd" "gvd" "mgv" "temphd" "chvd" "igvd" "img_vr" "gov2" "stv"
-    do
-    if test -f "data/raw/phagescope/merged_metadata.tsv"; then
-        echo "Already merged metadata, skipping..."
-    else
-        echo "Merging"
+if test -f "data/raw/phagescope/merged_metadata.tsv"; then
+    echo "Already merged metadata, skipping..."
+else
+    for DB in "genbank" "refseq" "ddbj" "embl" "phagesdb" "gpd" "gvd" "mgv" "temphd" "chvd" "igvd" "img_vr" "gov2" "stv";
+        do
+        echo "Merging $DB"
         if [ "$first" == true ]; then
-            cat $DB > data/raw/phagescope/merged_metadata.tsv
+            cat "data/raw/phagescope/${DB}_phage_meta_data.tsv" > data/raw/phagescope/merged_metadata.tsv
             first=false
         else
-            tail -n +2 $DB >> data/raw/phagescope/merged_metadata.tsv
+            tail -n +2 "data/raw/phagescope/${DB}_phage_meta_data.tsv" >> data/raw/phagescope/merged_metadata.tsv
         fi
-done 
+    done 
+fi
 echo "Done!"
