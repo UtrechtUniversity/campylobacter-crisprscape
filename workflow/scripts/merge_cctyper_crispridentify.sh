f61f6e0436132e5e4a89027dfe34c3cbd39ebe3c
Sam Nooij <s.nooij@uu.nl>
2026-01-07 14:36:18 +0100

    Add comments

--- workflow/scripts/merge_cctyper_crispridentify.sh
+++ workflow/scripts/merge_cctyper_crispridentify.sh
@@ -3,40 +3,58 @@ set -euo pipefail
 IFS=$'\n\t'
 
 first=True
+# For each CCTyper CRISPRs file...
 for summary in ${snakemake_input[cctyper]}
 do
+    # If it is the first, copy the whole file (including header)
     if [ "${first}" == True ]
     then
         cat "${summary}" > tmp_file1
         first=False
+    # otherwise, only take the 'contents', without header
     else
         tail -n +2 "${summary}" >> tmp_file1
     fi
+    # And write it all in one concatenated temporary file: 'tmp_file1'
 done
 
+# For CRISPRidentify, collect the header as variable
 header=$(head -n 1 ${snakemake_input[identify]} | cut -f 1,5,6,7,8,9,10,11,14 -d "," | tr "," "\t")
+# and write the contents of the 'complete summary' to another temporary file: 'tmp_file2'
 tail -n +2 ${snakemake_input[identify]} | cut -f 1,5,6,7,8,9,10,11,14 -d "," | tr "," "\t" > tmp_file2
+
 first=True
 
+# Start reading 'tmp_file1' = CCTyper CRISPRs
 while read line
 do
     if [ "${first}" == True ]
+    # For the first line...
     then
         first=False
+        # ...concatenate the header with CRISPRidentify's header
         echo -e "$line\t$header" > ${snakemake_output[table]}
     else
+
+    # For all other lines,
         sample=$(echo -e "${line}" | cut -f 1)
         start_cc=$(echo -e "${line}" | cut -f 3)
         start_id=$(expr "${start_cc}" + 1)
+        # see if the sample has a match in CRISPRidentify (tmp_file2)
         match=$(grep "${sample}_${start_id}" tmp_file2 || true)
 
         if [ -z "${match}" ]
         then
+        # If there's *no* match (-z = lenth 0)
             echo -e "${line}" >> ${snakemak_output[table]}
+            # Just copy the line from CCTyper
         else
+        # but if there is a match,
             while read line2
             do
                 if [ "${start_cc}" -lt 5000 ];
+                # check if the reported start position is greater than 5000 (the length of the flanking regions),
+                # and match the lines from CCTyper (line) and CRISPRidentify (match/line2) accordingly
                 then
                     echo -e "${line}\t${match}" >> ${snakemake_output[table]}
                 else
@@ -52,4 +70,6 @@ do
         fi
     fi
 done < tmp_file1
+
+# Remove the temporary files
 rm -f tmp_file1 tmp_file2
